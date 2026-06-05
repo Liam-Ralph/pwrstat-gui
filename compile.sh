008e5e0104bfb56a073f9f49f5bc8a1fc666c40c
Liam Ralph <liamralph42@gmail.com>
2026-06-05 10:09:05 -0400

    add flags to compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Parsing Flags
+
+git_branch=""
+python_version="3"
+
+while getopts "g:p:" flag; do
+    case $flag in
+        g) git_branch=$OPTARG ;;
+        p) python_version=$OPTARG ;;
+        *) echo -e "\nUsage: ./compile.sh -g <git branch> -p <python version>\n"; exit 1 ;;
+    esac
+done
+
 # Removing old executable if it exists
 
 rm -f pwrstat-gui
@@ -9,14 +22,19 @@ rm -f pwrstat-gui
 if [ ! -d "pwrstat-gui-clone" ]; then
     echo -e "\nCloning PwrStat GUI repository from GitHub...\n"
     git clone https://github.com/liam-ralph/pwrstat-gui pwrstat-gui-clone
+    if [ ! -z $git_branch ]; then
+        cd pwrstat-gui-clone
+        git checkout $git_branch
+        cd ..
+    fi
 fi
 
 # Setting up virtual environment if necessary
 
 if [ ! -d "compile-venv" ]; then
     echo -e "\nCreating virtual environment...\n"
-    python3 -m venv compile-venv
-    compile-venv/bin/python3 -m pip install --upgrade pyinstaller pillow setproctitle
+    python${python_version} -m venv compile-venv
+    compile-venv/bin/python$python_version -m pip install --upgrade pyinstaller pillow setproctitle
     mkdir compile-venv/pyinstaller-files
     cp pwrstat-gui-clone/main.py compile-venv/pyinstaller-files/pwrstat-gui.py
 fi
