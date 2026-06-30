f02e0900d253e25f6d4069195cdc645fa338fcad
Liam Ralph <liamralph42@gmail.com>
2026-06-30 15:25:22 -0400

    fix compile.sh and remove -cc flag

--- dev/compile.sh
+++ dev/compile.sh
@@ -2,24 +2,14 @@
 
 # Parsing Flags
 
-clean=0
-
 while getopts "c" flag; do
     case $flag in
-        c) ((clean++)) ;;
+        c) rm -rf compile-venv ;;
         *) 
-            echo -e "Usage: ./compile.sh\n" \
-                "[-c] <remove PyInstaller files before compilation>\n" \
-                "[-cc] <remove compile-venv before compilation>"
+            echo -e "Usage: ./compile.sh [-c] <remove PyInstaller files before compilation>\n"
             exit 1 ;;
     esac
 done
-
-if [ -d "compile-venv" ]; then
-    if (( clean == 1 )); then
-        rm -rf compile-venv/{pyinstaller-files, pwrstat-gui.spec, pyvenv.cfg}
-    elif (( clean == 2 )); then
-        rm -rf compile-venv
 
 # Setting up virtual environment if necessary
 
